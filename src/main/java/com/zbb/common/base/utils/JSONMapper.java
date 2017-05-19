584cd51b3b8aff1803665e171a020b8da549b2e7
feng <464091417@qq.com>
2017-05-19 19:17:00 +0800

    代码整理

--- src/main/java/com/zbb/common/base/utils/JSONMapper.java
+++ src/main/java/com/zbb/common/base/utils/JSONMapper.java
@@ -13,11 +13,14 @@ import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
  */
 @SuppressWarnings("deprecation")
 public class JSONMapper {
+	
 	private static ObjectMapper mapper = new ObjectMapper();
+	
 	static{
 		mapper.getSerializationConfig().setSerializationInclusion(Inclusion.NON_NULL);  
 		mapper.getDeserializationConfig().set(org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
 	}
+	
 	public static ObjectMapper getInstance(){
 		return mapper;
 	}
